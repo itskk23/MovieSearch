55b8ca87ad66ff006c196e1f1dd1b5240f2fab19
Kakha Kakhurashvili <kaxakaxurashvili@gmail.com>
2020-06-11 17:30:53 +0400

    movie search done

---
+++ proj/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SearchMovies from './SearchMovie';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies></SearchMovies>
+    </div>
+  );
+}
+
+export default App;
